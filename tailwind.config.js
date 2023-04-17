590a2fb821590108aeae75f8749ac29277bc6e1e
yazharasu <yazharasu@iamneo.ai>
2023-04-17 16:20:03 +0530

    tailwind css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.ts',
+    './src/pages/**/*.tsx',
+    './src/components/**/*.ts',
+    './src/components/**/*.tsx'
   ],
   theme: {
     extend: {
